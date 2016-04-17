badefe8d33e5f2da55f48137468e68ec9d873a50
Byeoungwook Kim <quddnr145@gmail.com>
2016-04-18 06:30:14 +0900

    add intcall() and fix to call intcall in put, puts

--- src/netos/boot/boot.c
+++ src/netos/boot/boot.c
@@ -1,5 +1,44 @@
-static void put(const char c) { asm volatile ("int $0x10" : : "a"(0x0e00 | c), "b"(0x0007)); }
+struct regs {
+  long edi, esi;
+  long ebp, esp;
+  long ebx, edx;
+  long ecx, eax;
+};
+
+/**
+ * interrupt call
+ */
+static void intcall(const int callno, struct regs& regs) {
+  asm volatile (
+    "int %0"
+    :
+    : "i"(callno), "a"(regs.eax), "b"(regs.ebx), "c"(regs.ecx), "d"(regs.edx), "S"(regs.esi), "D"(regs.edi)
+  );
+}
+
+/**
+ * print character
+ */
+static void put(const char c) {
+  struct regs regs;
+
+  regs.eax = 0x0e00 | c;
+  regs.ebx = 0x0007;
+  intcall(0x10, regs);
+}
+
+/**
+ * print string
+ */
+static void puts(const char* str) {
+  while(*str)
+    put(*str++);
+}
+
+/**
+ * bootloader main function
+ */
 void main() {
-  put('H'); put('i');
+  puts("Hi NetOS.git");
   while (1);
 }
